3f139b04bcdae35ecfda6784f9181dc609c7388e
Ayanfeoluwa Noah Akindele <akindeleayanfeoluwa@gmail.com>
2025-09-24 14:46:36 +0100

    minor mod

--- components/modals/ComposeEmailModal.tsx
+++ components/modals/ComposeEmailModal.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   Animated,
   Image,
   Keyboard,
   Modal,
   Pressable,
+  ScrollView,
   Text,
   TextInput,
   TouchableOpacity,
@@ -24,6 +25,53 @@ const ComposeEmailModal = ({
   setModalVisible: (visible: boolean) => void;
 }) => {
   const slideAnim = useRef(new Animated.Value(1000)).current;
+  const [showCC, setShowCC] = useState<boolean>(false);
+  const [showBCC, setShowBCC] = useState<boolean>(false);
+  const [recipients, setRecipients] = useState<any[]>([]);
+  const [ccrecipients, setCCRecipients] = useState<any[]>([]);
+  const [bccrecipients, setBCCRecipients] = useState<any[]>([]);
+  const [currentRecipientInput, setCurrentRecipientInput] =
+    useState<string>("");
+  const [currentCCRecipientInput, setCurrentCCRecipientInput] =
+    useState<string>("");
+  const [currentBCCRecipientInput, setCurrentBCCRecipientInput] =
+    useState<string>("");
+
+  const addRecipient = (cat: string) => {
+    if (cat === "recipient") {
+      const email = currentRecipientInput.trim();
+
+      if (email.length > 0 && !recipients.includes(email)) {
+        setRecipients([...recipients, email]);
+      }
+      setCurrentRecipientInput("");
+    } else if (cat === "ccrecipient") {
+      const email = currentCCRecipientInput.trim();
+
+      if (email.length > 0 && !ccrecipients.includes(email)) {
+        setCCRecipients([...ccrecipients, email]);
+      }
+      setCurrentCCRecipientInput("");
+    } else if (cat === "bccrecipient") {
+      const email = currentBCCRecipientInput.trim();
+
+      if (email.length > 0 && !bccrecipients.includes(email)) {
+        setBCCRecipients([...bccrecipients, email]);
+      }
+      setCurrentBCCRecipientInput("");
+    }
+  };
+
+  const removeRecipient = (email: string, cat: string) => {
+    if (cat === "recipient") {
+      setRecipients(recipients.filter((e) => e !== email));
+    } else if (cat === "ccrecipient") {
+      setCCRecipients(ccrecipients.filter((e) => e !== email));
+    } else if (cat === "bccrecipient") {
+      setBCCRecipients(bccrecipients.filter((e) => e !== email));
+    }
+  };
+
   useEffect(() => {
     if (modalVisible) {
       Animated.timing(slideAnim, {
@@ -34,86 +82,198 @@ const ComposeEmailModal = ({
     }
   }, [modalVisible]);
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <Modal
-        animationType="none"
-        transparent
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View className="flex-1 bg-black/40" />
+    <Modal
+      animationType="none"
+      transparent
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible(false)}
+    >
+      <View className="flex-1 bg-black/40" />
 
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <Animated.View
           style={{ transform: [{ translateY: slideAnim }] }}
           className="absolute bottom-0 h-[95%] w-full bg-white rounded-t-3xl p-5"
         >
-          <View className="flex flex-row justify-between items-center mt-6 border-b-2 border-[#f1f1f1] pb-3">
-            <View className="flex items-center flex-row">
-              <Pressable onPress={() => setModalVisible(false)}>
-                <XIcon />
-              </Pressable>
-              <Text className="ml-4 text-[20px] font-semibold">
-                Compose mail
-              </Text>
+          <ScrollView>
+            <View className="flex flex-row justify-between items-center mt-10 border-b-2 border-[#f1f1f1] pb-3">
+              <View className="flex items-center flex-row">
+                <Pressable onPress={() => setModalVisible(false)}>
+                  <XIcon />
+                </Pressable>
+                <Text className="ml-4 text-[20px] font-semibold">
+                  Compose mail
+                </Text>
+              </View>
+              <View className="flex flex-row items-center justify-between w-[40%]">
+                <ShareIcon />
+                <ScheduledIcon />
+                <EmailLockIcon />
+                <Image
+                  source={require("../../assets/images/ai-generate.png")}
+                  className="w-[44px] h-[44px] mt-3"
+                />
+              </View>
             </View>
-            <View className="flex flex-row items-center justify-between w-[40%]">
-              <ShareIcon />
-              <ScheduledIcon />
-              <EmailLockIcon />
-              <Image
-                source={require("../../assets/images/ai-generate.png")}
-                className="w-[44px] h-[44px] mt-3"
+
+            <View className="flex flex-row items-center mt-10">
+              <Text>To:</Text>
+              <TextInput
+                value={currentRecipientInput}
+                onChangeText={setCurrentRecipientInput}
+                onSubmitEditing={() => addRecipient("recipient")}
+                submitBehavior={"submit"}
+                className="border-b-2 border-[#D0D5DD] w-[90vw] ml-3 pb-2"
+                placeholder="Enter recipients"
+                placeholderTextColor="#9ca3af"
               />
             </View>
-          </View>
-
-          <View className="flex flex-row items-center mt-10">
-            <Text>To:</Text>
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              className="mt-3"
+            >
+              {recipients.map((email, i) => (
+                <View
+                  key={i}
+                  className="flex flex-row items-center bg-[#EEF0F4] px-2 py-2 rounded-full ml-2"
+                >
+                  <View className="bg-[#fff] flex items-center justify-center flex-row mr-2 rounded-full w-[20px] h-[20px]">
+                    <Text className="text-[12px]">
+                      {email.charAt(0).toUpperCase()}
+                    </Text>
+                  </View>
+                  <Text className="text-[12px]">{email}</Text>
+                  <TouchableOpacity
+                    className="ml-3"
+                    onPress={() => removeRecipient(email, "recipient")}
+                  >
+                    <Text>✕</Text>
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </ScrollView>
+            <View className="flex flex-row mt-10">
+              <Pressable onPress={() => setShowCC(!showCC)}>
+                <Text>{showCC ? "Remove CC" : "Add CC"}</Text>
+              </Pressable>
+              <Pressable className="ml-4" onPress={() => setShowBCC(!showBCC)}>
+                <Text>{showBCC ? "Remove BCC" : "Add BCC"}</Text>
+              </Pressable>
+            </View>
+            {showCC && (
+              <View>
+                <View className="flex flex-row items-center mt-10">
+                  <Text>CC:</Text>
+                  <TextInput
+                    value={currentCCRecipientInput}
+                    onChangeText={setCurrentCCRecipientInput}
+                    onSubmitEditing={() => addRecipient("ccrecipient")}
+                    submitBehavior={"submit"}
+                    className="border-b-2 border-[#D0D5DD] ml-3 w-[90vw] pb-2"
+                    placeholder="Enter CC recipients"
+                    placeholderTextColor="#9ca3af"
+                  />
+                </View>
+                <ScrollView
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  className="mt-3"
+                >
+                  {ccrecipients.map((email, i) => (
+                    <View
+                      key={i}
+                      className="flex flex-row items-center bg-[#EEF0F4] px-2 py-2 rounded-full ml-2"
+                    >
+                      <View className="bg-[#fff] flex items-center justify-center flex-row mr-2 rounded-full w-[20px] h-[20px]">
+                        <Text className="text-[12px]">
+                          {email.charAt(0).toUpperCase()}
+                        </Text>
+                      </View>
+                      <Text className="text-[12px]">{email}</Text>
+                      <TouchableOpacity
+                        className="ml-3"
+                        onPress={() => removeRecipient(email, "ccrecipient")}
+                      >
+                        <Text>✕</Text>
+                      </TouchableOpacity>
+                    </View>
+                  ))}
+                </ScrollView>
+              </View>
+            )}
+            {showBCC && (
+              <View>
+                <View className="flex flex-row items-center mt-10">
+                  <Text>BCC:</Text>
+                  <TextInput
+                    value={currentBCCRecipientInput}
+                    onChangeText={setCurrentBCCRecipientInput}
+                    onSubmitEditing={() => addRecipient("bccrecipient")}
+                    submitBehavior={"submit"}
+                    className="border-b-2 border-[#D0D5DD] ml-3 w-[90vw] pb-2"
+                    placeholder="Enter BCC recipients"
+                    placeholderTextColor="#9ca3af"
+                  />
+                </View>
+                <ScrollView
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  className="mt-3"
+                >
+                  {bccrecipients.map((email, i) => (
+                    <View
+                      key={i}
+                      className="flex flex-row items-center bg-[#EEF0F4] px-2 py-2 rounded-full ml-2"
+                    >
+                      <View className="bg-[#fff] flex items-center justify-center flex-row mr-2 rounded-full w-[20px] h-[20px]">
+                        <Text className="text-[12px]">
+                          {email.charAt(0).toUpperCase()}
+                        </Text>
+                      </View>
+                      <Text className="text-[12px]">{email}</Text>
+                      <TouchableOpacity
+                        className="ml-3"
+                        onPress={() => removeRecipient(email, "bccrecipient")}
+                      >
+                        <Text>✕</Text>
+                      </TouchableOpacity>
+                    </View>
+                  ))}
+                </ScrollView>
+              </View>
+            )}
+            <View className="flex flex-row items-center mt-10">
+              <Text>Subject:</Text>
+              <TextInput
+                className="border-b-2 border-[#D0D5DD] ml-3 w-[85%] pb-2"
+                placeholder="Enter subject"
+                placeholderTextColor="#9ca3af"
+              />
+            </View>
             <TextInput
-              className="border-b-2 border-[#D0D5DD] ml-3 w-[93%] pb-2"
-              placeholder="Enter recipients"
+              multiline
+              numberOfLines={10}
+              style={{ textAlignVertical: "top" }}
+              className="border border-[#D0D5DD] w-[100%] h-[30vh] mt-10 p-3 rounded-lg"
+              placeholder="Compose your mail..."
               placeholderTextColor="#9ca3af"
             />
-          </View>
-          <View className="flex flex-row mt-6">
-            <Pressable>
-              <Text>Add CC</Text>
-            </Pressable>
-            <Pressable className="ml-4">
-              <Text>Add BCC</Text>
-            </Pressable>
-          </View>
-          <View className="flex flex-row items-center mt-6">
-            <Text>Subject:</Text>
-            <TextInput
-              className="border-b-2 border-[#D0D5DD] ml-3 w-[85%] pb-2"
-              placeholder="Enter subject"
-              placeholderTextColor="#9ca3af"
-            />
-          </View>
 
-          <TextInput
-            multiline
-            numberOfLines={10}
-            style={{ textAlignVertical: "top" }}
-            className="border border-[#D0D5DD] w-[100%] h-[40vh] mt-6 p-3 rounded-lg"
-            placeholder="Compose your mail..."
-            placeholderTextColor="#9ca3af"
-          />
-
-          <TouchableOpacity
-            onPress={() => {
-              setModalVisible(false);
-            }}
-            className="m-auto w-full py-4 bg-[#3D4294] rounded-full"
-          >
-            <Text className="text-white text-center font-bold text-[16px]">
-              Send
-            </Text>
-          </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisible(false);
+              }}
+              className="mt-20 w-full py-4 bg-[#3D4294] rounded-full"
+            >
+              <Text className="text-white text-center font-bold text-[16px]">
+                Send
+              </Text>
+            </TouchableOpacity>
+          </ScrollView>
         </Animated.View>
-      </Modal>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </Modal>
   );
 };
 
